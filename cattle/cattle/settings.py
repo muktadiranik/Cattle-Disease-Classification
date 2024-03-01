f27e51a4df2bd1c3307f686d759d0b3c68aa44db
muktadiranik <muktadiranik@gmail.com>
2024-03-01 18:45:28 +0600

    multilanguage feature implemented

--- cattle/cattle/settings.py
+++ cattle/cattle/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -58,7 +59,10 @@ ROOT_URLCONF = "cattle.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "templates"],
+        "DIRS": [
+            os.path.join(BASE_DIR, "templates"),
+            os.path.join(BASE_DIR, "build")
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -126,6 +130,10 @@ MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
 STATIC_ROOT = BASE_DIR / "static"
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "build/static")
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- cattle/diseases/urls.py
+++ cattle/diseases/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from diseases.views import *
 
 urlpatterns = [
-    path("", IndexView.as_view(), name="index"),
+    path("", TemplateView.as_view(template_name="index.html"), name="index"),
+    path("about/", IndexView.as_view(), name="about"),
     path("api/", IndexAPIView.as_view(), name="api"),
     path("diseases/", DiseaseAPIView.as_view(), name="diseases"),
 ]
